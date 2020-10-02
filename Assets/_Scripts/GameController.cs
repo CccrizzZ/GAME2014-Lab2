afd5b8cf18a831909a4e9d5ae56d01e27087deb1
Christopher Liu <30523986+CccrizzZ@users.noreply.github.com>
2020-10-03 04:12:34 +0800

    ##

---
+++ Assets/_Scripts/GameController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    //public TMP_Text SceneLabel;
+    public TMP_Text LifeLabel;
+    public TMP_Text ScoreLabel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (Input.deviceOrientation)
+        {
+            case DeviceOrientation.LandscapeLeft:
+                LifeLabel.rectTransform.anchoredPosition = new Vector2(370.0f, -105f);
+                ScoreLabel.rectTransform.anchoredPosition = new Vector2(-349.0f, -105f);
+               
+                break;
+            case DeviceOrientation.LandscapeRight:
+                LifeLabel.rectTransform.anchoredPosition = new Vector2(248.0f, -105f);
+                ScoreLabel.rectTransform.anchoredPosition = new Vector2(-490.0f, -105f);
+           
+
+                break;
+            case DeviceOrientation.Portrait:
+                LifeLabel.rectTransform.anchoredPosition = new Vector2(248.0f, -185f);
+                ScoreLabel.rectTransform.anchoredPosition = new Vector2(-349.0f, -185f);
+               
+                break;
+            case DeviceOrientation.Unknown:
+                break;
+        }
+
+    }
+}
---
+++ Assets/_Scripts/TestButtonBehaviour.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class TestButtonBehaviour : MonoBehaviour
+{
+    public TMP_Text SceneLabel;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+ 
+    }
+
+    public void OnTestButtonPressed()
+    {
+        if (SceneLabel.text != "Test")
+        {
+            Debug.Log("Test Button Pressed");
+            SceneLabel.text = "Test";            
+        }
+        else
+        {
+            Debug.Log("Test Button Pressed");
+            SceneLabel.text = "Play Scene";      
+        }
+
+    }
+
+
+}
